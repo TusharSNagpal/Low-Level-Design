b282299b0bc0f0d565ac4a12cddb46679fcce435
TusharSNagpal <nagpal8795@gmail.com>
2023-06-08 01:08:35 +0530

    Factory-design-pattern

---
+++ 5. Factory Design Pattern/User.java
@@ -0,0 +1,13 @@
+import Shapes.DesignShape;
+
+public class User {
+    public static void main(String args[]) {
+        //main
+        ShapeFactory shape = new ShapeFactory(); //has-a
+        DesignShape circle = shape.getShape("CIRCLE");
+        DesignShape square = shape.getShape("SQUARE");
+        DesignShape rectangle = shape.getShape("RECTANGLE");
+
+        square.draw();
+    }
+}
